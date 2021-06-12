57afacd2ab1d163f48739068dc07713f020f14ca
marieJoeJoe <marieqoeqoe@gmail.com>
2021-06-12 20:12:31 +0800

    build coreMQTT

---
+++ include/core_mqtt_config.h
@@ -0,0 +1,9 @@
+#ifndef CORE_MQTT_CONFIG_h
+#define CORE_MQTT_CONFIG_h
+
+#define MQTT_DO_NOT_USE_CUSTOM_CONFIG
+/* A header file that defines trace macro can be included here. */
+
+#endif /* FREERTOS_CONFIG_H */
+
+
